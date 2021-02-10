313349042dca7a3f95ae7872cdd08316ee3f1208
Nihal Ahmad <nehalhmd14@gmail.com>
2021-02-10 15:28:45 +0530

    added landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import axios from 'axios';
-import { Provider } from 'react-redux';
-import store from './store/store';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
+import React from "react";
+import axios from "axios";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 import Dashboard from "./components/Dashboard/Dashboard";
-import Navbar from './components/Navbar/Navbar';
+import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/Login/Login";
 function App() {
-
-  axios.defaults.baseURL = 'https://opening-endpoints.herukoapp.com';
+  axios.defaults.baseURL = "https://opening-endpoints.herukoapp.com";
 
   return (
     <Provider store={store}>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/dashboard" exact component={Dashboard}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="/login" exact component={Login} />
         </Switch>
       </Router>
     </Provider>
