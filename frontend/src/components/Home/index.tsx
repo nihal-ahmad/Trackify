bf5814601a033233ee0eaf135c356be7772f7ad3
NIHAL AHMAD <nehalhmd14@gmail.com>
2021-02-12 22:17:15 +0530

    Add files via upload

---
+++ frontend/src/components/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from  "../Navbar/Navbar";
+import LandingPage from "./LandingPage/LandingPage";
+const Home = () => {
+  
+return(
+    <>
+<Navbar />
+<LandingPage />
+</>
+  );
+};
+
+export default Home;
